27c49e4538dc20cb4f28b87d4505b6255842cbd3
jet <jetbtc@outlook.com>
2015-01-22 23:59:16 +0100

    Add architecture

--- diggit-bot.user.js
+++ diggit-bot.user.js
@@ -11,85 +11,256 @@
     var status = ["NONE", "IN PROGRESS", "WON", "LOST"],
         gameId = 0,
         gameUpdates = window.jetstuff.gameUpdates = [],
-        rolls = [22, 12, 2],
+        tiles = [22, 12, 2],
         running = true;
 
-    socketio.on("update_game", function(data) {
-        if(data.ownerid === myuser.getID()) {
-            console.log(data, status[data.status]);
-        }
+    function Bot() {
+        return this.init();
+    }
 
-        if(data.ownerid === myuser.getID() && data.active) {
-            console.log('Active game:', data.id);
-            gameId = data.id;
+    $.extend(Bot.prototype, {
+        settings: {
+            autostart: false, // TODO
+            delay: 200,
+            historyLength: 200,
+            initialBet: 100,
+            maxBet: 1000,
+            maxStreak: -1, // TODO
+            mines: 13,
+            preroll: 0, // TODO
+            numRolls: 1,
+            randomRolls: true,
+            tiles: [20],
+            running: false,
+            resetOnWin: true,
+            resetOnLoss: false,
+            resetOnMaxbet: true,
+            winMultiplier: 0,
+            lossMultiplier: 2,
+        },
+        runningStats: {},
+        sessionStats: {
+            losses: 0,
+            lossStreak: 0,
+            profit: 0,
+            wagered: 0,
+            wins: 0,
+            winStreak: 0,
+        },
+        stats: {
+            losses: 0,
+            lossStreak: 0,
+            profit: 0,
+            wagered: 0,
+            wins: 0,
+            winStreak: 0,
+            history: [],
+        },
+        bet: 0,
+        halted: false,
+        finishStreak: false,
+        gameId: 0,
+        init: function() {
 
-            gameUpdates.push(data);
+            if(socketio) {
+                this.loadSettings();
+                this.loadStats();
 
-            if(data.status === 1) {
-                if(data.dug.length >= rolls.length) {
-                    console.log('cashing out!');
-                    cashout();
+                socketio.on("update_game", this.listen.bind(this));
+                console.info("bot ready");
+            } else {
+                console.warn("could not start bot!");
+            }
+
+            return this;
+        },
+        start: function(settings) {
+            var s = this.settings;
+
+            $.extend(s, settings);
+
+            this.running = true;
+            this.finishStreak = false;
+
+            this.bet = s.initialBet;
+
+            this.setTiles();
+
+            this.startGame();
+
+            return this;
+        },
+        halt: function(finishStreak) {
+            if(finishStreak) {
+                this.finishStreak = true;
+            }
+            this.running = false;
+
+            return this;
+        },
+        startGame: function() {
+            if(!this.running && !this.finishStreak) {
+                console.log('bot is halted. cannot start game.');
+                return this;
+            }
+
+            setTimeout(function() {
+                if(this.running || this.finishStreak) {
+                    socketio.emit('start_game', {
+                        mines: this.settings.mines,
+                        size: 25,
+                        betamount: this.bet,
+                        blockspecs: true
+                    });
+                    this.sessionStats.wagered += this.bet;
+
                 } else {
-                    console.log('digging..');
-                    socketio.emit("box_click", {
-                        gameid: data.id,
-                        boxid: rolls[data.dug.length]
-                    });
+                    console.log('halted. start prevented.');
                 }
-            } else if(data.status === 3) {
-                if(running) {
-                    console.log('LOST, starting a new game');
+            }.bind(this), this.settings.delay);
 
-                    newRolls();
-                    socketio.emit("start_game", {
-                        mines: 5,
-                        size: 25,
-                        betamount: data.betamount >= 3200 ? 100 : data.betamount * 2,
-                        blockspecs: true
-                    });
+            return this;
+        },
+        listen: function(data) {
+            if(!this.running && !this.finishStreak) {
+                return;
+            }
+
+            var history = this.stats.history;
+
+            if(data.ownerid === myuser.getID() && data.active) {
+                this.gameId = data.id;
+
+                if(data.status === 1) {
+                    this.play(data);
+                } else if(data.status === 3) {
+                    this.lostGame(data);
+                    history.push(data);
+
+                    this.sessionStats.losses++;
+                    this.sessionStats.lossStreak++;
+                    this.sessionStats.winStreak = 0;
+
+                    this.startGame();
+                } else if(data.status === 2) {
+                    this.wonGame(data);
+                    history.push(data);
+
+                    this.sessionStats.wins++;
+                    this.sessionStats.winStreak++;
+                    this.sessionStats.lossStreak = 0;
+
+                    this.startGame();
                 }
-            } else if(data.status === 2) {
-                if(running) {
-                    console.log('WON, starting a new game');
 
-                    newRolls();
-                    socketio.emit("start_game", {
-                        mines: 5,
-                        size: 25,
-                        betamount: 100,
-                        blockspecs: true
-                    });
+                if(history.length > this.settings.historyLength) history.shift();
+            }
+        },
+        play: function(data) {
+            if((!this.running && !this.finishStreak) || data.dug.length >= this.tiles.length) {
+                cashout();
+            } else {
+                socketio.emit("box_click", {
+                    gameid: data.id,
+                    boxid: this.tiles[data.dug.length]
+                });
+            }
+
+            return this;
+        },
+        wonGame: function(data) {
+            var s = this.settings,
+                bet = 0;
+
+            if(s.resetOnWin) {
+                this.endStreak();
+            } else {
+                bet = this.bet * s.winMultiplier;
+                if(bet > s.maxBet) {
+                    if(s.resetOnMaxbet) {
+                        console.log("Bet maxed. Resetting.");
+                        this.endStreak();
+                    } else {
+                        console.log("Bet maxed. Halting.");
+                        this.halt();
+                    }
+                } else {
+                    this.bet = bet;
                 }
             }
-        }
+        },
+        lostGame: function(data) {
+            var s = this.settings,
+                bet = 0;
+
+            if(s.resetOnLoss) {
+                this.endStreak();
+            } else {
+                bet = this.bet * s.lossMultiplier;
+                if(bet > s.maxBet) {
+                    if(s.resetOnMaxbet) {
+                        console.log("Bet maxed. Resetting.");
+                        this.endStreak();
+                    } else {
+                        console.log("Bet maxed. Halting.");
+                        this.halt();
+                    }
+                } else {
+                    this.bet = bet;
+                }
+            }
+        },
+        set: function(key, val) {
+            if(typeof key === "object") {
+                $.extend(this.settings, key);
+            } else {
+                this.settings[key] = val;
+            }
+        },
+        setTiles: function() {
+            if(this.settings.randomRolls) {
+                this.randomTiles(this.settings.numRolls);
+            } else {
+                this.tiles = this.settings.tiles.slice();
+            }
+
+            return this;
+        },
+        endStreak: function() {
+            var s = this.settings;
+
+            if(!this.running) {
+                this.finishStreak = false;
+            }
+            this.bet = s.initialBet;
+            this.setTiles();
+        },
+        randomTiles: function(length) {
+            var tiles = [];
+            for(var i=0; i<25; i++) {
+                tiles.push(i);
+            }
+
+            shuffle(tiles);
+            tiles.length = this.settings.numRolls;
+
+            this.tiles = tiles;
+
+            return this;
+        },
+        loadSettings: function() {},
+        saveSettings: function() {},
+        loadStats: function() {},
+        saveStats: function() {},
+        keepAlive: function() {},
     });
 
-    function newRolls() {
-        var newRolls = [];
-        for(var i=0; i<25; i++) {
-            newRolls.push(i);
-        }
-
-        shuffle(newRolls);
-        newRolls.length = 3 + Math.floor(3*Math.random());
-
-        rolls = newRolls;
-
-        return true;
-    }
-
-    function halt() {
-        running = false;
-        return !running;
-    }
-
+    jetstuff.bot = new Bot();
 
     //+ Jonas Raoni Soares Silva
     //@ http://jsfromhell.com/array/shuffle [v1.0]
     function shuffle(o){ //v1.0
         for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
         return o;
-    };
-
-    window.jetstuff.haltbot = halt;
+    }
 })();
